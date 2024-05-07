e06d5ae560b401e03daa27483b5be7232a288dee
icad08 <irsyadrm08@gmail.com>
2024-05-07 14:46:55 +0700

    add member class persegi panjang

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 
 class persegiPanjaang {
+        public:
+    int panjang, lebar;
+    int luasPersegiPanjang (int b, int a){
+        return a*b;
+    }
+};
 
-}
 
 
 int main(){
